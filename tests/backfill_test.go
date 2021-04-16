e3b0910e6172f695f604bbe5874ce318b6316f70
castaneai <castaneai@castaneai.net>
2021-04-16 19:13:09 +0900

    fix test

--- tests/backfill_test.go
+++ tests/backfill_test.go
@@ -19,7 +19,7 @@ func TestCreateTicketWithBackfill(t *testing.T) {
 		{Name: "test-pool", CreatedAfter: timestamppb.New(time.Now())},
 	}}
 
-	gameServerConnectionName := GameServerConnectionName("test-gs")
+	var allocatedGameServer *GameServer
 
 	ticket1 := mustCreateTicket(t, frontend, &pb.Ticket{})
 	{
@@ -33,9 +33,9 @@ func TestCreateTicketWithBackfill(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, int32(playersPerMatch-1), openSlots)
 
-		allocatedGameServer := AllocateGameServer(gameServerConnectionName, frontend)
+		allocatedGameServer = AllocateGameServer("test-gs", frontend)
 
-		mustAssignTickets(t, backend, matches[0], gameServerConnectionName)
+		mustAssignTickets(t, backend, matches[0], allocatedGameServer.ConnectionName())
 		assignment := mustAssignment(t, frontend, ticket1.Id, 3*time.Second)
 
 		allocatedGameServer.Connect(ticket1.Id)
@@ -55,7 +55,9 @@ func TestCreateTicketWithBackfill(t *testing.T) {
 		assert.Equal(t, int32(playersPerMatch-2), openSlots)
 
 		assignment := mustAssignment(t, frontend, ticket2.Id, 3*time.Second)
-		assert.Equal(t, string(gameServerConnectionName), assignment.Connection)
+		assert.Equal(t, string(allocatedGameServer.ConnectionName()), assignment.Connection)
+
+		allocatedGameServer.Connect(ticket2.Id)
 	}
 
 	ticket3 := mustCreateTicket(t, frontend, &pb.Ticket{})
@@ -70,6 +72,8 @@ func TestCreateTicketWithBackfill(t *testing.T) {
 		assert.Equal(t, int32(0), openSlots)
 
 		assignment := mustAssignment(t, frontend, ticket3.Id, 3*time.Second)
-		assert.Equal(t, string(gameServerConnectionName), assignment.Connection)
+		assert.Equal(t, string(allocatedGameServer.ConnectionName()), assignment.Connection)
+
+		allocatedGameServer.Connect(ticket3.Id)
 	}
 }
